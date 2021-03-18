c63fd724e22dcf15bcb7a874d6483a39a52670e6
Mohamed-Tarig <mohammedtarigfmcs@gmail.com>
2021-03-18 16:39:05 +0200

    commit second

--- estore/settings.py
+++ estore/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -126,3 +127,5 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+ALLOWED_HOSTS = ['.herokuapp.com',]
